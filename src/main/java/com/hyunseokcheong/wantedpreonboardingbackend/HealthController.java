746db4bc66cc8ede24ba284f9ccda84cb1140132
HyunseokCheong <hyunseokcheong@gmail.com>
2023-08-04 12:55:27 +0900

    test: cicd

--- src/main/java/com/hyunseokcheong/wantedpreonboardingbackend/HealthController.java
+++ src/main/java/com/hyunseokcheong/wantedpreonboardingbackend/HealthController.java
@@ -3,11 +3,14 @@ package com.hyunseokcheong.wantedpreonboardingbackend;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class HealthController {
     
     @GetMapping("/health")
     public String health() {
-        return "health";
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return "현재 서버시간은 " + localDateTime + "입니다. \n";
     }
 }
